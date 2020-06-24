dba50211d8caeec0af8a6185aee49e3f65b0af1f
Akshant Lanjewar <akshant.lanjewar@gmail.com>
2020-06-24 14:03:29 -0700

    added working node list, working on attributes

--- LunarEngine/public/LunarGUI/LunarGuiPane.h
+++ LunarEngine/public/LunarGUI/LunarGuiPane.h
@@ -1,30 +1,62 @@
 #include "../LunarLogger.h"
 
 #include <string>
+#include <vector>
+#include <tuple>
+#include <algorithm>
 #include <cstdio>
 #include <cerrno>
+#include <sstream>
+#include <numeric>
+#include <iterator>
 
 //TEST
 /*
     * PANE
-    * BOX 1
-    *   width = 20% height = 100% SET width OFFSET 20% for 100% of height
-    *   CHILD TODO (Child offset resets)
-    * 
-    * Box 2
-    *   width = 80%; height = 20px
-    *   Offset = 20%;
-    * Box 3
-    *   width = 300px height = 30px
-    *   offset = 20%
+    * container 1
+    *   INLINE
+    *   width (only 72 px)
+    *   height 100%
+    * container 2
+    *   INLINE
+    *   width (100% - 72px) (thats what a space does, takes up the rest of space)
+    *   height is 100%
+    *   CHILDREN
+    *       container 2
+    *           BLOCK
+    *           Height = 20px SETS Parents children offset height to +20px
+    *       container 3
+    *           BLOCK
+    *           width = 300px
+    *           height = 30px; SETS Parents children offset height to +50 px, +20 from above, and +30 from this
 */
 
 namespace LunarGUI {
+    struct GUINode {
+        bool endTag;
+        std::string nodeName;
+    };
+
+    struct GUINodeAttrib {
+        bool endTag;
+        bool hasAttrib;
+
+        std::string nodeName;
+        std::vector<std::tuple<std::string, std::string>> attributes;
+    };
+
     class GUIPane {
     public:
         GUIPane();
         ~GUIPane();
 
         void CreatePane(std::string loc);
+
+    private:
+        void CreateNodeList(std::string contents);
+
+    private:
+        std::vector<GUINode> nodeList;
+        std::vector<GUINodeAttrib> nodeAttribList;
     };
 }
--- LunarEngine/source/LunarGUI/LunarGuiPane.cpp
+++ LunarEngine/source/LunarGUI/LunarGuiPane.cpp
@@ -24,6 +24,68 @@ namespace LunarGUI {
             fileContents = contents;
         }
 
+        fileContents.erase(std::remove(fileContents.begin(), fileContents.end(), '\n'), fileContents.end());
+        fileContents.erase(std::remove(fileContents.begin(), fileContents.end(), '\t'), fileContents.end());
         
+        CreateNodeList(fileContents);
+    }
+
+    void GUIPane::CreateNodeList(std::string contents) {
+        std::string tmpNode;
+        bool startTag = false;
+
+        for(std::string::size_type i = 0; i < contents.size(); i++) {
+            if(contents[i] == '<') {
+                startTag = true;
+                continue;
+            }
+            if(contents[i] == '>') {
+                startTag = false;
+
+                GUINode _node = {};
+                _node.nodeName = tmpNode;
+                if(tmpNode[0] == '/')
+                    _node.endTag = true;
+                else
+                    _node.endTag = false;
+                nodeList.push_back(_node);
+
+                tmpNode = "";
+                continue;
+            }
+                
+            if(!startTag && contents[i] == ' ')
+                continue;
+            if(startTag)
+                tmpNode += contents[i];
+        }
+
+        for(auto _node : nodeList) {
+            std::stringstream ss(_node.nodeName);
+            
+            std::istream_iterator<std::string> begin(ss);
+            std::istream_iterator<std::string> end;
+
+            std::vector<std::string> attributes(begin, end);
+            
+            if(attributes.size() > 1) {
+                attributes.erase(attributes.begin());
+                std::cout << attributes[0] << std::endl;
+                std::vector<std::tuple<std::string, std::string>> _tmpAttributes;
+
+                GUINodeAttrib _nodeAttrib = {};
+                _nodeAttrib.hasAttrib = true;
+                _nodeAttrib.endTag = _node.endTag;
+
+                _nodeAttrib.nodeName = _node.nodeName;
+                _nodeAttrib.attributes = _tmpAttributes;
+            } else {
+                GUINodeAttrib _nodeAttrib = {};
+                _nodeAttrib.endTag = _node.endTag;
+                _nodeAttrib.hasAttrib = false;
+
+                _nodeAttrib.nodeName = _node.nodeName;
+            }
+        }
     }
 }
